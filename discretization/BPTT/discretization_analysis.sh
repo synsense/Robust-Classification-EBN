5445ea88a0fe3448435f73be0f61d8260161a89c
Sergio <smgsolinas@gmail.com>
2020-09-13 22:53:07 +0200

    Set to run odd BPTT on zemo.

--- discretization/BPTT/discretization_analysis.sh
+++ discretization/BPTT/discretization_analysis.sh
@@ -11,7 +11,7 @@ fi
 
 if [ $(uname -n) = "zemo" ]
 then
-    for NETWORK_IDX in 0 2 4 6 8
+    for NETWORK_IDX in 1 3 5 7 9
     do
 	python evaluate_bptt.py --verbose 0 --network-idx $NETWORK_IDX 2>&1 | tee analisys_$NETWORK_IDX.log
     done

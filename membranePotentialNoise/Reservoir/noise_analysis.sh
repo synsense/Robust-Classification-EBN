79448daca85b85e8357241529fd79dcefc141dd5
Julian Buechel <julian.buechell@aictx.ai>
2020-09-21 11:13:45 +0200

    Merge

--- membranePotentialNoise/Reservoir/noise_analysis.sh
+++ membranePotentialNoise/Reservoir/noise_analysis.sh
@@ -3,5 +3,5 @@ git checkout feature/NetworkADS-Jax
 cd ~/Documents/RobustClassificationWithEBNs/membranePotentialNoise/Reservoir
 for NETWORK_IDX in 0 1 2 3 4 5 6 7 8 9
 do
-python analyse_reservoir.py --network-idx $NETWORK_IDX 2>&1 | tee analyse_reservoir_$NETWORK_IDX.log
+python analyse_reservoir.py --network-idx $NETWORK_IDX
 done

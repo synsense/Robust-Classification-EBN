b347a73e1088abc412e18369c319ee25fbe6d3a3
Julian Buechel <julian.buechell@aictx.ai>
2020-10-07 12:53:54 +0200

    Hot

--- mismatch/BPTT/mismatch_analysis.sh
+++ mismatch/BPTT/mismatch_analysis.sh
@@ -3,5 +3,5 @@ git checkout feature/NetworkADS-Jax
 cd ~/Documents/RobustClassificationWithEBNs/mismatch/BPTT
 for NETWORK_IDX in 0 1 2 3 4 5 6 7 8 9
 do
-python hey_snips_bptt_mismatch.py --verbose 0 --num-trials 50 --network-idx $NETWORK_IDX 
+python hey_snips_bptt_mismatch.py --verbose 0 --num-trials 10 --network-idx $NETWORK_IDX 
 done

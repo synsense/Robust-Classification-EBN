59811e56d184be234eccb2113cf8062a04516960
Sergio Solinas <smgsolinas@gmail.com>
2020-11-18 22:15:12 +0100

    Mis sim analysis setup.

--- mismatch/Reservoir/mismatch_analysis.sh
+++ mismatch/Reservoir/mismatch_analysis.sh
@@ -1,7 +1,7 @@
 cd ~/rockpool
 git checkout feature/NetworkADS-Jax
 cd ~/Documents/RobustClassificationWithEBNs/mismatch/Reservoir
-for NETWORK_IDX in 6 7 8 9
+for NETWORK_IDX in 3
 do
 python hey_snips_reservoir_mismatch.py --num-trials 10 --network-idx $NETWORK_IDX 2<&1 | tee analysis_$NETWORK_IDX.log
 done

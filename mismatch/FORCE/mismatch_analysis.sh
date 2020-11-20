0576059bd75e954d2231bf5a53d64b0d781bd6b0
Julian Buechel <julian.buechell@aictx.ai>
2020-11-20 10:40:55 +0100

    Force mm analysis

--- mismatch/FORCE/mismatch_analysis.sh
+++ mismatch/FORCE/mismatch_analysis.sh
@@ -1,7 +1,7 @@
 cd ~/rockpool
 git checkout feature/FORCE-Jax
 cd ~/Documents/RobustClassificationWithEBNs/mismatch/FORCE
-for NETWORK_IDX in 0 1 2 3 4 5 6 7 8 9
+for NETWORK_IDX in 6 7 8
 do
 python hey_snips_force_mismatch.py --verbose 0 --num-trials 10 --network-idx $NETWORK_IDX
 done
